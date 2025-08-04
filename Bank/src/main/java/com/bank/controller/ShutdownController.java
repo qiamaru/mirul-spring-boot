563079f220d6a0c32ce9743b84ebfdd8a6ad0ebe
Amirul Afiq Shahdan <amirulafiq.shahdan@gmail.com>
2025-08-04 14:03:41 +0800

    DevOps Practical 8

--- Bank/src/main/java/com/bank/controller/ShutdownController.java
+++ Bank/src/main/java/com/bank/controller/ShutdownController.java
@@ -1,5 +1,7 @@
 package com.bank.controller;
 
+import javax.swing.Spring;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -8,19 +10,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.annotation.PreDestroy;
+
 @RestController
 public class ShutdownController implements ApplicationContextAware {
-    
+    // INFO - implements ApplicationContextAware
+    // This allow access to the ApplicationContext (Spring container) of this app.”
+
     private ApplicationContext context;
-    
+    // INFO - Stores the application context instance (Spring's internal container
+    // managing beans, configs, etc.).
+
     @GetMapping("/shutdownContext")
     public void shutdownContext() {
         ((ConfigurableApplicationContext) context).close();
     }
+    // Here’s the core logic:
+    // The Spring application context is cast to ConfigurableApplicationContext,
+    // which provides the .close() method.
+    // Calling .close() will:
+    // Trigger @PreDestroy hooks
+    // Release resources
+    // Terminate background threads
+    // Shut down the application gracefully
+    // This is the equivalent of "asking Spring to shut itself down from within."
 
     @Override
     public void setApplicationContext(ApplicationContext ctx) throws BeansException {
         this.context = ctx;
-        
     }
+    // This is the method from the ApplicationContextAware interface.
+    // When Spring initializes this controller, it automatically calls this method
+    // and passes the current application context (ctx) into it.
+    // This is how the controller gets access to the app context.
 }
